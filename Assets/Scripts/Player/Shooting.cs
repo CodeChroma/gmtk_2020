1020df23e4eba40b4c13e9abaf3b0477ec3c97a5
Sophie Atkins <tileyratkins@gmail.com>
2020-07-12 18:07:19 +0200

    Laser swapping added

--- Assets/Scripts/Player/Shooting.cs
+++ Assets/Scripts/Player/Shooting.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class Shooting : MonoBehaviour
 {
     public Transform firePoint;
-    public GameObject shotPrefab;
+    public GameObject[] laserPrefabs;
+    private int currentLaser;
 
     public float shotForce = 20f;
+
+    void Start()
+    {
+        currentLaser = 0;
+    }
 
     // Update is called once per frame
     void Update()
@@ -16,11 +22,21 @@ public class Shooting : MonoBehaviour
         {
             Shoot();
         }
+
+        if (Input.GetKeyDown("f"))
+        {
+            SwapWeapon();
+        }
+    }
+
+    void SwapWeapon()
+    {
+        currentLaser = (currentLaser + 1) % laserPrefabs.Length;
     }
 
     void Shoot() 
     {
-        GameObject shot = Instantiate(shotPrefab, firePoint.position, firePoint.rotation);
+        GameObject shot = Instantiate(laserPrefabs[currentLaser], firePoint.position, firePoint.rotation);
         Rigidbody2D rigidbody = shot.GetComponent<Rigidbody2D>();
         rigidbody.AddForce(firePoint.up * shotForce, ForceMode2D.Impulse);
     }
