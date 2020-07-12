7569809e4b27f1298bb8fc96ea5baf8ffbca05d4
halvardssm <halvard@simplyundoable.com>
2020-07-12 20:24:02 +0200

    adding start and quit buttons

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+  
+public class StartGame : MonoBehaviour
+{
+    public void GoToLevel(string level="1_Stars")
+    {
+        SceneManager.LoadScene(level);
+    }
+}
