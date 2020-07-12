0de6aecc7a4f17ad59a7c298368f92c257b92c52
Sophie Atkins <tileyratkins@gmail.com>
2020-07-12 17:06:41 +0200

    tech background, added all laser prefabs, WIP collision + explosion animation on hit

---
+++ Assets/Scripts/Lasers/Collision.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    public GameObject hitEffect;
+
+    /// <summary>
+    /// Sent when an incoming collider makes contact with this object's
+    /// collider (2D physics only).
+    /// </summary>
+    /// <param name="other">The Collision2D data associated with this collision.</param>
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Instantiate(hitEffect, transform.position, Quaternion.identity);
+        Destroy(hitEffect, 5f);
+        Destroy(gameObject);
+    }
+}
